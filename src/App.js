79be1b10815fc0d61fdb9d755aaa46a38824d8fb
Numan Ahmed <mdnmnahmed@gmail.com>
2020-12-30 09:14:19 +0530

    removed all previous codes, start from fresh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Employee from './Employee';
 
-//Creating ContextAPI
+import {Provider} from './Context';
 
 class App extends Component {
     state = {
@@ -10,10 +10,23 @@ class App extends Component {
         salary: 15000,
     }
 
+
+    increaseSalary = () => {
+        this.setState({
+            salary: this.state.salary + 5000
+        });
+    }
+
     render() {
+        const data = {
+            stateData: this.state,
+            increaseSalary: this.increaseSalary
+        }
+
         return (
             <>
                 <h2>App Component</h2>
+                <Employee />
             </>
         );
     }
--- src/Salary.js
+++ src/Salary.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { MyContext } from './App';
 
 class Salary extends Component {
 
