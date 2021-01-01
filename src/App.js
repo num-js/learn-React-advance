9e428fc6d7d23d8dc7ea6fc1fc4b6a2bfea54a64
Numan Ahmed <mdnmnahmed@gmail.com>
2021-01-02 00:00:23 +0530

    Dark/Light Mode

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React, { Component } from 'react';
-import Employee from './Employee';
-
-import { Provider } from './Context';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import './App.css';
+import ThemeContextProvider from './contexts/ThemContext';
 
 class App extends Component {
-    state = {
-        name: 'Numan',
-        pro: 'MERN Stack Developer',
-        salary: 15000,
-    }
-
-
-    increaseSalary = () => {
-        this.setState({
-            salary: this.state.salary + 5000
-        });
-    }
-
     render() {
-        const data = {
-            stateData: this.state,
-            increaseSalary: this.increaseSalary
-        }
-
         return (
-            <>
-                <Provider value={data}>
-                    <h2>App Component</h2>
-                    <Employee />
-                </Provider>
-            </>
+            <div className="App">
+                <ThemeContextProvider>
+                    <Navbar />
+                    <BookList />
+                </ThemeContextProvider>
+            </div>
         );
     }
 }
+
 export default App;
