813fd837bebd5cfc06b1cc19f55e8a117604fa70
Numan Ahmed <mdnmnahmed@gmail.com>
2021-05-01 23:51:03 +0530

    SetState With Objects

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import { useState } from 'react';
 
 const App = () => {
-    const [counter1, setCounter1] = useState(0);
-    const [counter2, setCounter2] = useState(0);
+
+    const [counter, _setCounter] = useState({
+        counter1: 1,
+        counter2: 2
+    });
+
+    const setCounter = (newCounter) => {
+        _setCounter(oldCounter => {
+            return {
+                ...oldCounter,
+                ...newCounter
+            }
+        })
+    }
 
     return (
         <>
-            <div
-                onClick={() => {
-                    setCounter1(counter1 + 1);
+            <div>
+                <h1>SetState With Objects</h1>
 
-                    /**
-                     * This Will not Update again the State
-                     */
-                    setTimeout(() => {
-                        setCounter1(counter1 + 1);
-                    }, 1000)
-                }}
-            >
+                <h2
+                    onClick={() => {
+                        setCounter({
+                            counter1: counter.counter1 + 1
+                        })
+                    }}
+                >Increase Counter1 --- {counter.counter1}</h2>
 
-                <h1>Counter-1 Magic : {counter1}</h1>
-            </div>
 
-            <br />
-            <br />
+                <h2
+                    onClick={() => {
+                        setCounter({
+                            counter2: counter.counter2 + 1
+                        })
+                    }}
+                >Increase Counter2 --- {counter.counter2}</h2>
 
-            <div
-                onClick={() => {
-                    setCounter2(counter2 + 1);
-
-                    /**
-                     * This Will Update again the State
-                     */
-                    setTimeout(() => {
-                        setCounter2((currentValue) => currentValue + 1);
-                    }, 1000)
-                }}
-            >
-
-                <h1>Counter- 2 Magic : {counter2}</h1>
             </div>
         </>
     );
