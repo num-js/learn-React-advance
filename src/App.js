bc777547e9e0e06bb827236cde3fe25982a3317e
Numan Ahmed <mdnmnahmed@gmail.com>
2021-05-01 23:29:12 +0530

    useState Magics with setTimeOut

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import { useState } from 'react';
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
+    const [counter1, setCounter1] = useState(0);
+    const [counter2, setCounter2] = useState(0);
 
     return (
         <>
             <div
-                onClick={() => setCounter((previousValue) => {
-                    console.log('prevoiusValue: ', previousValue);
-                    return previousValue + 1
-                })}
+                onClick={() => {
+                    setCounter1(counter1 + 1);
+
+                    /**
+                     * This Will not Update again the State
+                     */
+                    setTimeout(() => {
+                        setCounter1(counter1 + 1);
+                    }, 1000)
+                }}
             >
 
-                <h1>Counter: {counter}</h1>
+                <h1>Counter-1 Magic : {counter1}</h1>
+            </div>
+
+            <br />
+            <br />
+
+            <div
+                onClick={() => {
+                    setCounter2(counter2 + 1);
+
+                    /**
+                     * This Will Update again the State
+                     */
+                    setTimeout(() => {
+                        setCounter2((currentValue) => currentValue + 1);
+                    }, 1000)
+                }}
+            >
+
+                <h1>Counter- 2 Magic : {counter2}</h1>
             </div>
         </>
     );
