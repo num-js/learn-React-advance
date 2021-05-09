fa1b015aa52b99e0abf00a0beeac9ad49948998d
Numan Ahmed <mdnmnahmed@gmail.com>
2021-05-09 17:55:36 +0530

    use Provider to App component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-import { useState } from 'react';
+import React from 'react';
+import CounterComponent from './Component/CounterComponent';
+import store from './Redux/store';
+
+import { Provider } from 'react-redux';
+
+store.subscribe(() => {
+    console.log(store);
+})
 
 const App = () => {
-
-    const [counter, _setCounter] = useState({
-        counter1: 1,
-        counter2: 2
-    });
-
-    const setCounter = (newCounter) => {
-        _setCounter(oldCounter => {
-            return {
-                ...oldCounter,
-                ...newCounter
-            }
-        })
-    }
-
     return (
         <>
-            <div>
-                <h1>SetState With Objects</h1>
-
-                <h2
-                    onClick={() => {
-                        setCounter({
-                            counter1: counter.counter1 + 1
-                        })
-                    }}
-                >Increase Counter1 --- {counter.counter1}</h2>
-
-
-                <h2
-                    onClick={() => {
-                        setCounter({
-                            counter2: counter.counter2 + 1
-                        })
-                    }}
-                >Increase Counter2 --- {counter.counter2}</h2>
-
-            </div>
+            <Provider store={store}>
+                <CounterComponent />
+            </Provider>
         </>
     );
 }
