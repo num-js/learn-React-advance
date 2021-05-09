85569b9351218c7754c6965b432b0e0b131e3980
Numan Ahmed <mdnmnahmed@gmail.com>
2021-05-09 18:04:20 +0530

    Removed previous codes

--- src/Redux/Reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import changeNum from "./counterReducer";
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-    changeNum
-    // second Reducer if available
-});
-
-export default rootReducer;
